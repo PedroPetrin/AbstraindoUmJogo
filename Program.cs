5287bf608e74d5e81fa0df3a841ea04a9f5d2cc5
PedroPetrin <pedropetrin08@gmail.com>
2022-01-27 22:23:50 -0300

    Abstraindo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AbstraindoUmJogo.src.Entities;
+
+namespace AbstraindoUmJogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+
+            Console.WriteLine(wizard.Attack(1));
+            Console.WriteLine(wizard.Attack(7));
+
+        }
+    }
+}
